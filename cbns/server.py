59233ecb34966550220e7fcb03ae323f0fcc853b
CUB3D <callumthom11@gmail.com>
2019-04-24 23:45:55 +0100

    switched to aioredis

--- cbns/server.py
+++ cbns/server.py
@@ -1,11 +1,14 @@
 from starlette.applications import Starlette
 from starlette.websockets import WebSocketDisconnect
 from starlette.responses import JSONResponse
-import redis
+import aioredis
+from aioredis.pubsub import Receiver
 
 app = Starlette()
 
-redis_server = redis.Redis(host='redis', port=6379, password='')
+# redis_server = redis.Redis(host='redis', port=6379, password='')
+
+REDIS_URL = "redis://redis:6379"
 
 
 @app.websocket_route('/')
@@ -15,39 +18,47 @@ async def ws_(ws):
     await ws.close()
 
 
-@app.websocket_route('/poll/{id}')
-async def poll_for_notifications(ws):
-    id = ws.path_params['id']
-    await ws.accept()
-    redis_server.sadd('connected_devices', id)
-
-    ps = redis_server.pubsub()
-    ps.subscribe(f"channel_device_{id}", "channel_device_common")
-    print(f"Starting poll for {id}")
-
+async def poll_channel_manager(ws, channel):
     try:
-        while True:
-            msg = ps.get_message()
+        while await channel.wait_message():
+            msg = await channel.get(encoding='utf-8')
 
             if msg:
                 print(f"Got message: {msg}")
-                if msg['type'] != 'subscribe':
-                    data = msg['data'].decode("utf-8")
 
-                    await ws.send_text(data)
+                await ws.send_text(msg)
     except WebSocketDisconnect as e:
         print(e)
     finally:
         print(f"Client {id} dc")
 
+
+@app.websocket_route('/poll/{id}')
+async def poll_for_notifications(ws):
+    id = ws.path_params['id']
+    await ws.accept()
+
+    redis_server = await aioredis.create_redis(REDIS_URL)
+    await redis_server.sadd('connected_devices', id)
+
+    # multi_reciever = Receiver()
+    # await redis_server.subscribe
+
+
+    channel_device, channel_common = await redis_server.subscribe(f"channel_device_{id}", "channel_device_common")
+    print(f"Starting poll for {id}")
+
+    await poll_channel_manager(ws, channel_common)
+
     await ws.close()
-    redis_server.srem('connected_devices', id)
+    await redis_server.srem('connected_devices', id)
 
 
 @app.route('/post/{data}', methods=['POST'])
 async def data_post(req):
     data = req.path_params['data']
-    redis_server.publish('channel_device_common', data)
+    redis_server = await aioredis.create_redis('redis://redis:6379')
+    await redis_server.publish('channel_device_common', data)
     return JSONResponse({
         'Status': 1
     })
