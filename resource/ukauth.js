36383747566fdc19030f79a22cf0af2a1121a6f3
CUB3D <callumthom11@gmail.com>
2019-07-06 23:44:48 +0100

    Started work on auth library

--- resource/ukauth.js
+++ resource/ukauth.js
@@ -1,7 +1,7 @@
 let ukauth = {
    oneClickLogin: function(appDescription) {
       // window.location.href = "https://auth.cub3d.pw/auth/app/" + appDescription.APP_ID;
-      window.location.href = "http://localhost:8081/auth/app/" + appDescription.APP_ID;
+      window.location.href = "http://localhost:8080/auth/app/" + appDescription.APP_ID;
 
    }
 };
