62371d6a2279668902d8f93e79d1e1ef4d3ba7be
CUB3D <callumthom11@gmail.com>
2020-01-19 16:08:59 +0000

    Updated script for cbns

--- cbns/send_notification.py
+++ cbns/send_notification.py
@@ -2,21 +2,37 @@ import requests
 from urllib.parse import quote
 import json
 from sys import argv
+from pprint import pprint
 
 if len(argv) < 3:
-    print("Usage: send_notifciation: <title> <message>")
+    print("Usage: send_notifciation: [-r <key:value>...] [<title> <message>]")
     exit(1)
 
-title = argv[-2]
-content = argv[-1]
+json_obj = {}
 
-json_string = quote(json.dumps({
-    "targetAppID": "pw.cub3d.uk",
-    "message": {
-        "title": title,
-        "content": content,
+if "-r" in argv:
+    print("Working in raw mode")
+    payload = [{"key": x.split(":")[0], "value": x.split(":")[1]} for x in argv[2:]]
+    json_obj = {
+        "targetAppID": "pw.cub3d.uk",
+        "dataPayload": payload
     }
-}).strip())
+else:
+    title = argv[-2]
+    content = argv[-1]
+
+    json_obj = {
+    "targetAppID": "pw.cub3d.uk",
+        "message": {
+            "title": title,
+            "content": content,
+        }
+    }
+
+
+print("Sending:")
+pprint(json_obj)
+json_string = quote(json.dumps(json_obj)).strip()
 
 print(json_string)
 
