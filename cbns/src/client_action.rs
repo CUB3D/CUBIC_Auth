01da4d5526775aec67f606d23e416cd1820cc59f
CUB3D <callumthom11@gmail.com>
2020-03-27 03:13:11 +0000

    CBNS migrated to seperate project

--- cbns/src/client_action.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use crate::notification_definition::Notification;
-
-#[derive(Serialize, Deserialize, Debug)]
-pub struct ClientAction {
-    pub action_name: String,
-    pub target: Option<String>,
-    pub notification_payload: Option<Notification>
-}
--- cbns/src/endpoint/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod socket_poll;
-pub mod root;
-pub mod post_device;
-pub mod post_channel;
