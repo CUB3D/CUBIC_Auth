fd68e4d97dd9ef12669efbaffb42cb1a14ef1e19
CUB3D <callumthom11@gmail.com>
2020-03-27 03:11:56 +0000

    Added basic debug ui

--- cbns/src/endpoint/root.rs
+++ cbns/src/endpoint/root.rs
@@ -1,6 +1,18 @@
 use actix_web::HttpResponse;
 use actix_web::Error;
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    connected_devices: u32
+}
 
 pub fn root_handler() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().body("<h1>404 Not Found</h1>"))
+
+    let tpl = IndexTemplate {
+        connected_devices: 0
+    }.render().unwrap();
+
+    Ok(HttpResponse::Ok().body(tpl))
 }
